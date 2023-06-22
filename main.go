608009848251553dbb42af97cab650c00a11338d
Gabrielgqa <gabrielgqa@gmail.com>
2023-06-22 16:23:31 +0000

    base

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-  var titular string = "Gabriel"
-  var numeroAgencia int = 589
-  var numeroConta int = 123456
-  var saldo float64 = 125.50
+	var titular string = "Gabriel"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.50
 
-  var titular2 string = "Carlos"
-  var numeroAgencia2 int = 589
-  var numeroConta2 int = 123457
-  var saldo2 float64 = 100.50
-  
-  fmt.Println(titular, numeroAgencia, numeroConta, saldo)
-  fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+	var titular2 string = "Carlos"
+	var numeroAgencia2 int = 589
+	var numeroConta2 int = 123457
+	var saldo2 float64 = 100.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
 }
